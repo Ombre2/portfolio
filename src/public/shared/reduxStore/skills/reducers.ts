531ecc5d04602bf01e70654c36ede782c2b75250
Ombre2 <johnylegenie@gmail.com>
2024-06-09 13:03:04 +0300

    IMPROVE - ajouter  react-loading-skeleton et mettre une annimation a l'affichage du skills

--- src/public/shared/reduxStore/skills/reducers.ts
+++ src/public/shared/reduxStore/skills/reducers.ts
@@ -4,11 +4,13 @@ import { ISkill } from 'public/shared/types/Skill';
 // Define a type for the slice state
 interface SkillsState {
   listSkill: ISkill[];
+  loading: boolean;
 }
 
 // Define the initial state using that type
 const initialState: SkillsState = {
-  listSkill: []
+  listSkill: [],
+  loading: false
 };
 
 export const skillsSlice = createSlice({
@@ -17,10 +19,13 @@ export const skillsSlice = createSlice({
   reducers: {
     setListSkill: (state, action: PayloadAction<ISkill[]>) => {
       state.listSkill = action.payload;
+    },
+    setLoading: (state, action: PayloadAction<boolean>) => {
+      state.loading = action.payload;
     }
   }
 });
 
-export const { setListSkill } = skillsSlice.actions;
+export const { setListSkill, setLoading } = skillsSlice.actions;
 
 export default skillsSlice.reducer;
