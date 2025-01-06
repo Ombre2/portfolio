9a05a36b9caa808192224a31374ccc999f92a653
Johny <johnylegenie@gmail.com>
2025-01-06 18:28:38 +0300

    IMPROVE - ajouter des metaData pour les SEO et quand on cpier coller un lien sur les reseaux sociaux

--- src/public/pages/home/components/IntroduiceMyself.tsx
+++ src/public/pages/home/components/IntroduiceMyself.tsx
@@ -15,6 +15,12 @@ const IntroduceMyself: React.FC = () => {
           {/* Taille du texte ajustée pour mobile et desktop */}
           <h1 className="text-3xl sm:text-4xl lg:text-7xl leading-relaxed font-black">{t('HOME.INTRODUCTION')}</h1>
           <p className="mt-2 text-base sm:text-lg lg:text-xl">{t('HOME.DESCRIPTION')}</p>
+          <div className="relative group inline-block cursor-pointer mt-4">
+            <div className="bg-gradient-to-r from-blue-500 via-purple-500 to-blue-500 text-white px-8 py-3 rounded-md shadow-lg cursor-pointer transition-all duration-300 ease-in-out transform hover:scale-105 text-center">
+              TELECHARGER CV
+            </div>
+            <div className="absolute inset-0 bg-blue-500 rounded-md blur-lg opacity-50 group-hover:opacity-80 transition-opacity duration-300"></div>
+          </div>
         </div>
       </div>
 
